196a699c7e34b2a8190465fa2f6d1519e816b7fb
Pedro Kalil <kalilgamer0@gmail.com>
2018-08-17 00:24:24 +0000

    sanders: import system prop * Also fix some derps

--- lineage.mk
+++ lineage.mk
@@ -27,7 +27,7 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
-PRODUCT_NAME := sacred_sanders
+PRODUCT_NAME := lineage_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
